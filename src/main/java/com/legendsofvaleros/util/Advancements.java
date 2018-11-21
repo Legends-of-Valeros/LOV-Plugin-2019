0e31cd9bf1ef2eaf09d89e8a1dda624653c5bf4e
stumblinbear <stumblinbear@gmail.com>
2018-11-21 13:07:58 -0600

    Remove advancements on boot

--- src/main/java/com/legendsofvaleros/util/Advancements.java
+++ src/main/java/com/legendsofvaleros/util/Advancements.java
@@ -4,12 +4,22 @@ import com.legendsofvaleros.LegendsOfValeros;
 import io.chazza.advancementapi.AdvancementAPI;
 import io.chazza.advancementapi.FrameType;
 import io.chazza.advancementapi.Trigger;
+import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
+import org.bukkit.advancement.Advancement;
+
+import java.util.Iterator;
 
 public class Advancements {
 	public static AdvancementAPI PARENT;
 	
 	public static void onEnable() {
+		// Just to be safe, lets remove all existing advancements, first.
+		Iterator<Advancement> i = Bukkit.advancementIterator();
+		i.forEachRemaining((a) -> {
+			Bukkit.getUnsafe().removeAdvancement(a.getKey());
+		});
+
 		PARENT = AdvancementAPI.builder(new NamespacedKey(LegendsOfValeros.getInstance(), "lov/parent"))
 	                .title("Welcome to Valeros")
 	                .description("Begin your adventure.")
