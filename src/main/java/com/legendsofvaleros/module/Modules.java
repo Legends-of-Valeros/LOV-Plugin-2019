b8193601ec2d4b862890e5eb75c021a6b70f9e76
CrystallDEV <m.hasselder@gmx.de>
2018-11-25 05:50:15 +0100

    fixed modules nullpointer exception

--- src/main/java/com/legendsofvaleros/module/Modules.java
+++ src/main/java/com/legendsofvaleros/module/Modules.java
@@ -12,30 +12,57 @@ import java.util.*;
 import java.util.logging.Logger;
 
 public class Modules {
-    private static Logger getLogger() { return LegendsOfValeros.getInstance().getLogger(); }
+    private static Logger getLogger() {
+        return LegendsOfValeros.getInstance().getLogger();
+    }
 
-    /** All available modules (ignoring "enabled") */
+    /**
+     * All available modules (ignoring "enabled")
+     */
     private static Map<String, Class<? extends Module>> available = new HashMap<>();
 
-    /** List of modules that are enabled. This does not mean that they are loaded. */
+    /**
+     * List of modules that are enabled. This does not mean that they are loaded.
+     */
     private static Set<Class<? extends Module>> enabled = new HashSet<>();
 
-    /** Module dependency pairs. Pair<optional, module> */
+    /**
+     * Module dependency pairs. Pair<optional, module>
+     */
     private static Multimap<Class<? extends Module>, Pair<Boolean, Class<? extends Module>>> dependencies = HashMultimap.create();
 
 
-    /** The packages that modules reside in. */
+    /**
+     * The packages that modules reside in.
+     */
     private static Map<String, Class<? extends Module>> packages = new HashMap<>();
     private static Map<Class<? extends Module>, Module> modules = new LinkedHashMap<>();
-    public static Collection<Module> getLoadedModules() { return modules.values(); }
-    public static boolean isEnabled(Class<? extends Module> clazz) { return modules.containsKey(clazz); }
+
+    public static Collection<Module> getLoadedModules() {
+        return modules.values();
+    }
+
+    public static boolean isEnabled(Class<? extends Module> clazz) {
+        return modules.containsKey(clazz);
+    }
 
 
-    /** Schedulers for each module. */
+    /**
+     * Schedulers for each module.
+     */
     private static Map<Class<? extends Module>, InternalScheduler> schedulers = new LinkedHashMap<>();
-    public static Collection<InternalScheduler> getSchedulers() { return schedulers.values(); }
-    public static InternalScheduler getScheduler(Class<? extends Module> clazz) { return schedulers.get(clazz); }
-    public static InternalScheduler getScheduler(Module module) { return schedulers.get(module.getClass()); }
+
+    public static Collection<InternalScheduler> getSchedulers() {
+        return schedulers.values();
+    }
+
+    public static InternalScheduler getScheduler(Class<? extends Module> clazz) {
+        return schedulers.get(clazz);
+    }
+
+    public static InternalScheduler getScheduler(Module module) {
+        return schedulers.get(module.getClass());
+    }
 
     /**
      * Attempt to load all modules that are marked as "enabled."
@@ -51,18 +78,18 @@ public class Modules {
         List<Class<? extends Module>> items = new ArrayList<>(enabled);
 
         // While we still have modules waiting to be enabled
-        while(items.size() > 0) {
+        while (items.size() > 0) {
             List<Class<? extends Module>> remaining = new ArrayList<>();
             boolean emitted = false;
 
             // Loop through each waiting module
             mods:
-            for(Class<? extends Module> clazz : items) {
+            for (Class<? extends Module> clazz : items) {
                 Collection<Pair<Boolean, Class<? extends Module>>> depends = dependencies.get(clazz);
 
                 // If their dependencies aren't loaded, add to remaining then try the next module
-                for(Pair<Boolean, Class<? extends Module>> depend : depends) {
-                    if(!isDependencyMet(depend)) {
+                for (Pair<Boolean, Class<? extends Module>> depend : depends) {
+                    if (!isDependencyMet(depend)) {
                         remaining.add(clazz);
                         continue mods;
                     }
@@ -83,14 +110,14 @@ public class Modules {
             }
 
             // Unable to load any of the remaining modules. Bail.
-            if(!emitted) {
+            if (!emitted) {
                 i++;
 
                 // Output unloaded (enabled) modules and the dependencies that prevented them from loading
-                for(Class<? extends Module> clazz : remaining) {
+                for (Class<? extends Module> clazz : remaining) {
                     List<String> unmet = new ArrayList<>();
-                    for(Pair<Boolean, Class<? extends Module>> depend : dependencies.get(clazz)) {
-                        if(!isDependencyMet(depend))
+                    for (Pair<Boolean, Class<? extends Module>> depend : dependencies.get(clazz)) {
+                        if (!isDependencyMet(depend))
                             unmet.add(depend.getRight().getSimpleName());
                     }
 
@@ -105,7 +132,7 @@ public class Modules {
 
         getLogger().fine("Loaded " + modules.size() + " modules");
 
-        if(enabled.size() > modules.size())
+        if (enabled.size() > modules.size())
             getLogger().severe("Failed to load " + (enabled.size() - modules.size()) + " modules!");
     }
 
@@ -114,10 +141,10 @@ public class Modules {
      */
     private static boolean isDependencyMet(Pair<Boolean, Class<? extends Module>> depend) {
         // Ignore optional dependencies if they're not enabled.
-        if(depend.getLeft() && !enabled.contains(depend.getRight())) return true;
+        if (depend.getLeft() && !enabled.contains(depend.getRight())) return true;
 
         // If the module is not yet loaded, add it to the remaining modules.
-        if(!modules.containsKey(depend.getRight()))
+        if (!modules.containsKey(depend.getRight()))
             return false;
 
         return true;
@@ -127,7 +154,7 @@ public class Modules {
      * Attempt to load a module.
      */
     public static void loadModule(Class<? extends Module> clazz) throws IllegalAccessException, InstantiationException {
-        if(modules.containsKey(clazz))
+        if (modules.containsKey(clazz))
             throw new IllegalStateException("Attempted to initialize an already loaded module!");
 
         Module moduleToLoad = clazz.newInstance();
@@ -145,20 +172,18 @@ public class Modules {
      */
     public static void unloadModules() {
         List<Module> toUnload = new ArrayList<>();
-
-        modules.values().forEach(module -> toUnload.add(module));
+        toUnload.addAll(modules.values());
 
         // Unload modules in reverse
-        for(int i = toUnload.size() - 1; i >= 0; i--) {
-            Module moduleToUnload = modules.get(toUnload.get(i));
-
+        for (int i = toUnload.size() - 1; i >= 0; i--) {
+            Module moduleToUnload = toUnload.get(i);
             getLogger().info("(" + (i + 1) + "/" + toUnload.size() + ") Unloading " + moduleToUnload.getName() + "...");
             moduleToUnload.onUnload();
 
             // stopping the modules thread
             try {
                 InternalScheduler scheduler = moduleToUnload.getScheduler();
-                if(scheduler.isAlive())
+                if (scheduler.isAlive())
                     scheduler.join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -166,7 +191,6 @@ public class Modules {
         }
 
         enabled.clear();
-
         modules.clear();
         packages.clear();
         schedulers.clear();
@@ -176,20 +200,20 @@ public class Modules {
         available.put(clazz.getSimpleName(), clazz);
 
         DependsOn[] depends = clazz.getAnnotationsByType(DependsOn.class);
-        for(DependsOn dep : depends)
+        for (DependsOn dep : depends)
             dependencies.put(clazz, new ImmutablePair<>(dep.optional(), dep.value()));
     }
 
     public static String getModulePackage(Class<?> clazz) {
         String pack = clazz.getName();
         String[] ss = pack.split("com\\.legendsofvaleros\\.modules\\.", 2);
-        if(ss.length < 2) return null;
+        if (ss.length < 2) return null;
         return "com.legendsofvaleros.modules." + ss[1].split("\\.", 2)[0];
     }
 
     public static Class<? extends Module> getModuleClass(Class<?> clazz) {
         String pack = getModulePackage(clazz);
-        if(pack != null && packages.containsKey(pack))
+        if (pack != null && packages.containsKey(pack))
             return packages.get(pack);
         return null;
     }
