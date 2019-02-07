94f08029f733555e32364d68ea03c028b05c6b69
stumblinbear <stumblinbear@gmail.com>
2019-02-07 13:06:37 -0600

    Remove unnecessary annotation

--- src/main/java/com/legendsofvaleros/api/APITest.java
+++ src/main/java/com/legendsofvaleros/api/APITest.java
@@ -2,7 +2,6 @@ package com.legendsofvaleros.api;
 
 import com.legendsofvaleros.api.annotation.ModuleRPC;
 
-@ModuleRPC
 public interface APITest {
     Promise<Object> testRPC();
     Promise<Object> ping();
