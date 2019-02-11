a59ab7d95e5514915f631a4b2f3c6bc5bead6cb8
stumblinbear <stumblinbear@gmail.com>
2019-02-11 01:59:09 -0600

    Add decoders

--- src/main/java/com/legendsofvaleros/modules/mobs/MobsController.java
+++ src/main/java/com/legendsofvaleros/modules/mobs/MobsController.java
@@ -1,6 +1,8 @@
 package com.legendsofvaleros.modules.mobs;
 
+import com.google.gson.JsonDeserializer;
 import com.legendsofvaleros.LegendsOfValeros;
+import com.legendsofvaleros.api.APIController;
 import com.legendsofvaleros.module.Module;
 import com.legendsofvaleros.module.annotation.DependsOn;
 import com.legendsofvaleros.module.annotation.ModuleInfo;
@@ -19,12 +21,14 @@ import com.legendsofvaleros.modules.mobs.listener.MobListener;
 import com.legendsofvaleros.modules.mobs.pl8.MobHealthbarManager;
 import com.legendsofvaleros.modules.npcs.NPCsController;
 import com.legendsofvaleros.modules.parties.PartiesController;
+import net.citizensnpcs.api.trait.trait.Equipment;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.EquipmentSlot;
 
 @DependsOn(CombatEngine.class)
 @DependsOn(Characters.class)
@@ -54,6 +58,38 @@ public class MobsController extends Module {
         this.instance = this;
 
         this.api = new MobsAPI();
+
+        APIController.getInstance().getGsonBuilder()
+            .registerTypeAdapter(Mob.StatsMap.class, (JsonDeserializer<Mob.StatsMap>) (json, typeOfT, context) -> {
+                return new Mob.StatsMap(context.deserialize(json.getAsJsonObject(), Mob.StatsMap.StatData[].class));
+            })
+            .registerTypeAdapter(Equipment.EquipmentSlot.class, (JsonDeserializer<Equipment.EquipmentSlot>) (json, typeOfT, context) -> {
+                String name = json.getAsString().toUpperCase();
+
+                if (name.equals("OFFHAND"))
+                    name = "OFF_HAND";
+
+                try {
+                    return Equipment.EquipmentSlot.valueOf(name);
+                } catch (Exception e) {
+                    try {
+                        switch (EquipmentSlot.valueOf(name)) {
+                            case HEAD:
+                                return Equipment.EquipmentSlot.HELMET;
+                            case CHEST:
+                                return Equipment.EquipmentSlot.CHESTPLATE;
+                            case LEGS:
+                                return Equipment.EquipmentSlot.LEGGINGS;
+                            case FEET:
+                                return Equipment.EquipmentSlot.BOOTS;
+                            default:
+                                return null;
+                        }
+                    } catch (Exception e1) {
+                        throw new RuntimeException("Unknown equipment slot. Offender: " + name);
+                    }
+                }
+            });
 
         getLogger().info("AI will update all entities over the course of " + LegendsOfValeros.getInstance().getConfig().getInt("ai-update-smear", 20) + " ticks.");
         ai = new BehaviorEngine(getConfig().getInt("ai-update-smear", 10));
