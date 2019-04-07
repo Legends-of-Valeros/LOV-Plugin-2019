316bff65be4720bf6481d44a1c1f4413077e39b0
CrystallDEV <m.hasselder@gmx.de>
2019-04-07 11:26:40 +0200

    Added zone activate and deactivate events

---
+++ src/main/java/com/legendsofvaleros/modules/zones/event/ZoneDeactivateEvent.java
@@ -0,0 +1,27 @@
+package com.legendsofvaleros.modules.zones.event;
+
+import com.legendsofvaleros.modules.zones.core.Zone;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class ZoneDeactivateEvent extends Event {
+    private static final HandlerList handlers = new HandlerList();
+
+    private Zone zone;
+
+    public Zone getZone() {
+        return zone;
+    }
+
+    public ZoneDeactivateEvent(Zone zone) {
+        this.zone = zone;
+    }
+
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
