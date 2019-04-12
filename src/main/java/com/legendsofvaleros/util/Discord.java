31f85ef376a28d11bea92500119acb6ce02a6b30
CrystallDEV <m.hasselder@gmx.de>
2019-04-12 12:59:46 +0200

    Extended Discord.class to easily send messages into a channel id and / or name

--- src/main/java/com/legendsofvaleros/util/Discord.java
+++ src/main/java/com/legendsofvaleros/util/Discord.java
@@ -3,6 +3,7 @@ package com.legendsofvaleros.util;
 import com.google.common.util.concurrent.FutureCallback;
 import de.btobastian.javacord.DiscordAPI;
 import de.btobastian.javacord.Javacord;
+import de.btobastian.javacord.entities.Channel;
 import de.btobastian.javacord.entities.Server;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
@@ -16,12 +17,15 @@ public class Discord {
     public static Server SERVER;
     public static String TAG;
 
+    private static Channel logsChannel;
+
     public static void onEnable() {
         ConfigurationSection config = Utilities.getInstance().getConfig().getConfigurationSection("discord");
 
         TAG = config.getString("tag");
-        if(TAG == null)
+        if (TAG == null) {
             TAG = "unknown";
+        }
 
         API = Javacord.getApi(config.getString("token"), true);
         API.connect(new FutureCallback<DiscordAPI>() {
@@ -46,18 +50,94 @@ public class Discord {
 
     public static class ConnectedEvent extends Event {
         private static final HandlerList handlers = new HandlerList();
-        @Override public HandlerList getHandlers() { return handlers; }
-        public static HandlerList getHandlerList() { return handlers; }
+
+        @Override public HandlerList getHandlers() {
+            return handlers;
+        }
+
+        public static HandlerList getHandlerList() {
+            return handlers;
+        }
 
         final Server server;
-        public Server getServer() { return server; }
+
+        public Server getServer() {
+            return server;
+        }
 
         final DiscordAPI api;
-        public DiscordAPI getAPI() { return api; }
+
+        public DiscordAPI getAPI() {
+            return api;
+        }
 
         public ConnectedEvent(Server server, DiscordAPI api) {
             this.server = server;
             this.api = api;
         }
     }
+
+    /**
+     * Sends a message to the #logs channel
+     * @param message
+     */
+    public static void sendLogMessage(String message) {
+        Channel logs = Discord.getLogsChannel();
+        if (logs == null) {
+            MessageUtil.sendSevereException("Discord", "Could not find the logs channel. Check the permission");
+            return;
+        }
+        logs.sendMessage(message);
+    }
+
+    /**
+     * Sends a message to the given discord channel id
+     * @param message
+     * @param channelId
+     */
+    public static void sendMessageToChannelId(String message, String channelId) {
+        Channel channel = Discord.API.getChannelById(channelId);
+        if (channel == null) {
+            MessageUtil.sendException("Discord", "Could not find channel by id -" + channelId);
+            return;
+        }
+        channel.sendMessage(message);
+    }
+
+    /**
+     * Sends a message to the given discord channel name
+     * @param message
+     * @param channelName
+     */
+    public static void sendMessageToChannelName(String message, String channelName) {
+        Channel channelTo = null;
+        for (Channel channel : Discord.API.getChannels()) {
+            if (channel.getName().equalsIgnoreCase(channelName)) {
+                channelTo = channel;
+                break;
+            }
+        }
+        if (channelTo == null) {
+            MessageUtil.sendException("Discord", "Could not find channel by name -" + channelName);
+            return;
+        }
+
+        channelTo.sendMessage(message);
+    }
+
+    /**
+     * Returns the id of the logs channel
+     * @return
+     */
+    public static Channel getLogsChannel() {
+        if (logsChannel == null) {
+            for (Channel channel : Discord.API.getChannels()) {
+                if (channel.getName().equalsIgnoreCase("logs")) {
+                    logsChannel = channel;
+                }
+            }
+        }
+        return logsChannel;
+    }
+
 }
