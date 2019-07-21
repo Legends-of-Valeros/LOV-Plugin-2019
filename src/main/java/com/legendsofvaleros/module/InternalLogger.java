01be388bb115eab9b6022e4ca9aff4aa160d1184
CrystallDEV <m.hasselder@gmx.de>
2019-07-21 13:42:27 +0200

    Fixed Logger not applying formatting strings

--- src/main/java/com/legendsofvaleros/module/InternalLogger.java
+++ src/main/java/com/legendsofvaleros/module/InternalLogger.java
@@ -1,75 +1,50 @@
 package com.legendsofvaleros.module;
 
 import com.legendsofvaleros.LegendsOfValeros;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.plugin.PluginLogger;
+import org.jetbrains.annotations.NotNull;
 
-import java.lang.reflect.Field;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
-public class InternalLogger extends PluginLogger {
-    Field superField;
-
-    String prefix = "";
+public class InternalLogger extends Logger {
+    private String prefix = "";
 
     public InternalLogger() {
-        super(LegendsOfValeros.getInstance());
-
-        try {
-            superField = PluginLogger.class.getDeclaredField("pluginName");
-            superField.setAccessible(true);
-            prefix += superField.get(this);
-        } catch(Exception e) { e.printStackTrace(); }
-
+        super("LOV", null);
+        this.setParent(LegendsOfValeros.getInstance().getServer().getLogger());
         this.setLevel(Level.ALL);
     }
 
     public InternalLogger(Module module) {
         this();
-
-        try {
-            prefix += "[" + module.getName() + "] ";
-            superField.set(this, prefix);
-        } catch(Exception e) { e.printStackTrace(); }
-
+        prefix += "[" + module.getName() + "] ";
         this.setLevel(Level.ALL);
     }
 
     @Override
-    public void log(LogRecord logRecord) {
-        ChatColor color = null;
+    public void log(@NotNull LogRecord logRecord) {
+        ChatColor color;
+        Level level = logRecord.getLevel();
 
-        if(Bukkit.getConsoleSender() != null) {
-            Level level = logRecord.getLevel();
-
-            if(level == Level.INFO) {
-                color = ChatColor.WHITE;
-            }else if(level == Level.FINEST) {
-                color = ChatColor.AQUA;
-            }else if(level == Level.FINER) {
-                color = ChatColor.BLUE;
-            }else if(level == Level.FINE) {
-                color = ChatColor.GREEN;
-            }else if(level == Level.WARNING) {
-                color = ChatColor.RED;
-            }else if(level == Level.SEVERE) {
-                color = ChatColor.DARK_RED;
-            }else{
-                color = ChatColor.WHITE;
-            }
+        if (level == Level.INFO) {
+            color = ChatColor.WHITE;
+        } else if (level == Level.FINEST) {
+            color = ChatColor.AQUA;
+        } else if (level == Level.FINER) {
+            color = ChatColor.BLUE;
+        } else if (level == Level.FINE) {
+            color = ChatColor.GREEN;
+        } else if (level == Level.WARNING) {
+            color = ChatColor.RED;
+        } else if (level == Level.SEVERE) {
+            color = ChatColor.DARK_RED;
+        } else {
+            color = ChatColor.WHITE;
         }
 
-        logRecord.setMessage((color != null ? color : "") + logRecord.getMessage());
-
-        if(Bukkit.getConsoleSender() != null) {
-            logRecord.setMessage(prefix + logRecord.getMessage());
-
-            Bukkit.getConsoleSender().sendMessage(logRecord.getMessage());
-            return;
-        }
-
+        logRecord.setMessage(prefix + color + logRecord.getMessage());
         super.log(logRecord);
     }
 }
