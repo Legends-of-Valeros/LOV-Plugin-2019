ca28b6fe3b649dd7e8d40306245d1b5a8f38842d
CrystallDEV <m.hasselder@gmx.de>
2019-07-24 12:39:35 +0200

    Test push for discord (bot) notification

---
+++ src/main/java/com/legendsofvaleros/modules/arena/Arena.java
@@ -0,0 +1,7 @@
+package com.legendsofvaleros.modules.arena;
+
+/**
+ * Created by Crystall on 07/24/2019
+ */
+public class Arena {
+}
---
+++ src/main/java/com/legendsofvaleros/modules/arena/ArenaController.java
@@ -0,0 +1,10 @@
+package com.legendsofvaleros.modules.arena;
+
+import com.legendsofvaleros.module.ModuleListener;
+
+/**
+ * Created by Crystall on 07/24/2019
+ */
+public class ArenaController extends ModuleListener {
+
+}
