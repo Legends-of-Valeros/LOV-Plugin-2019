7ba799bdf033499ee7ffbb6fb3f778acc1b0330b
CrystallDEV <m.hasselder@gmx.de>
2019-07-26 00:01:51 +0200

    Fixed a bug that caused characters not being created

--- src/main/java/com/legendsofvaleros/modules/characters/core/PlayerInventoryData.java
+++ src/main/java/com/legendsofvaleros/modules/characters/core/PlayerInventoryData.java
@@ -7,70 +7,75 @@ import org.bukkit.Bukkit;
 import org.bukkit.inventory.ItemStack;
 
 public class PlayerInventoryData implements InventoryData {
-	public interface InventoryMethod {
-		Promise<String> encode(ItemStack[] contents);
-		Promise<ItemStack[]> decode(String data);
-	}
-	
-	public static InventoryMethod method = new InventoryMethod() {
-		@Override
-		public Promise<String> encode(ItemStack[] contents) {
-			return Promise.make("");
-		}
+    public interface InventoryMethod {
+        Promise<String> encode(ItemStack[] contents);
 
-		@Override
-		public Promise<ItemStack[]> decode(String data) {
-			return Promise.make(new ItemStack[0]);
-		}
-		
-	};
-	
-	private String inventoryData;
-	
-	public PlayerInventoryData() { }
-	public PlayerInventoryData(String inventoryData) {
-		this.inventoryData = inventoryData;
-	}
-	
-	@Override
-	public Promise<Boolean> onInvalidated(PlayerCharacter pc) {
-		return saveInventory(pc);
-	}
-	
-	@Override
-	public Promise<Boolean> saveInventory(PlayerCharacter pc) {
-		return method.encode(pc.getPlayer().getInventory().getContents()).onSuccess(val -> {
-			inventoryData = val.orElseGet(null);
-		}).next(val -> Promise.make(val.isPresent()));
-	}
+        Promise<ItemStack[]> decode(String data);
+    }
 
-	@Override
-	public void initInventory(PlayerCharacter pc) {
-		Bukkit.getServer().getPluginManager().callEvent(new PlayerCharacterInventoryFillEvent(pc, true));
-	}
+    public static InventoryMethod method = new InventoryMethod() {
+        @Override
+        public Promise<String> encode(ItemStack[] contents) {
+            return Promise.make("");
+        }
 
-	@Override
-	public Promise<Boolean> loadInventory(PlayerCharacter pc) {
-		pc.getPlayer().getInventory().clear();
+        @Override
+        public Promise<ItemStack[]> decode(String data) {
+            return Promise.make(new ItemStack[0]);
+        }
 
-		if(inventoryData != null) {
-			return method.decode(inventoryData).onSuccess(val -> {
-				pc.getPlayer().getInventory().setContents(val.orElse(new ItemStack[0]));
+    };
 
-				Bukkit.getServer().getPluginManager().callEvent(new PlayerCharacterInventoryFillEvent(pc, false));
-			}, Characters.getInstance().getScheduler()::sync)
-					.next(al -> Promise.make(al.isPresent()));
-		}
+    private String inventoryData;
 
-		return Promise.make(true);
-	}
+    public PlayerInventoryData() {
+    }
 
-	@Override
-	public void onDeath(PlayerCharacter pc) {
-		
-	}
-	
-	public String getData() {
-		return inventoryData;
-	}
+    public PlayerInventoryData(String inventoryData) {
+        this.inventoryData = inventoryData;
+    }
+
+    @Override
+    public Promise<Boolean> onInvalidated(PlayerCharacter pc) {
+        return saveInventory(pc);
+    }
+
+    @Override
+    public Promise<Boolean> saveInventory(PlayerCharacter pc) {
+        return method.encode(pc.getPlayer().getInventory().getContents()).onSuccess(val -> {
+            inventoryData = val.orElseGet(null);
+        }).next(val -> Promise.make(val.isPresent()));
+    }
+
+    @Override
+    public void initInventory(PlayerCharacter pc) {
+        Characters.getInstance().getScheduler().executeInSpigotCircle(() -> {
+            Bukkit.getServer().getPluginManager().callEvent(new PlayerCharacterInventoryFillEvent(pc, true));
+        });
+    }
+
+    @Override
+    public Promise<Boolean> loadInventory(PlayerCharacter pc) {
+        pc.getPlayer().getInventory().clear();
+
+        if (inventoryData != null) {
+            return method.decode(inventoryData).onSuccess(val -> {
+                pc.getPlayer().getInventory().setContents(val.orElse(new ItemStack[0]));
+
+                Bukkit.getServer().getPluginManager().callEvent(new PlayerCharacterInventoryFillEvent(pc, false));
+            }, Characters.getInstance().getScheduler()::sync)
+                    .next(al -> Promise.make(al.isPresent()));
+        }
+
+        return Promise.make(true);
+    }
+
+    @Override
+    public void onDeath(PlayerCharacter pc) {
+
+    }
+
+    public String getData() {
+        return inventoryData;
+    }
 }
