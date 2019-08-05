586bd9478acc09c42f45497962466510b075bf51
CrystallDEV <m.hasselder@gmx.de>
2019-08-05 23:25:25 +0200

    Arena prototype

---
+++ src/main/java/com/legendsofvaleros/modules/arena/commands/ArenaCommands.java
@@ -0,0 +1,24 @@
+package com.legendsofvaleros.modules.arena.commands;
+
+/**
+ * Created by Crystall on 08/04/2019
+ */
+
+import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.Description;
+import co.aikar.commands.annotation.Subcommand;
+import org.bukkit.entity.Player;
+
+/**
+ * Created by Crystall on 08/02/2019
+ */
+@CommandAlias("arena")
+public class ArenaCommands {
+
+    @Subcommand("quit")
+    @Description("Quits out of the arena and gives up")
+    public void onQuit(Player player) {
+        //TODO get arena and quit out of it
+    }
+
+}
