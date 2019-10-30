3164bf511079c2375d4f56e32f34514a998691bd
CrystallDEV <m.hasselder@gmx.de>
2019-10-30 14:55:58 +0100

    Added basic priests skill classes and specialisation (no skill implementation)

---
+++ src/main/java/com/legendsofvaleros/modules/skills/core/priest/core/SkillDispel.java
@@ -0,0 +1,32 @@
+package com.legendsofvaleros.modules.skills.core.priest.core;
+
+import com.legendsofvaleros.modules.classes.EntityClass;
+import com.legendsofvaleros.modules.classes.skills.Skill;
+import com.legendsofvaleros.modules.combatengine.api.CombatEntity;
+import org.bukkit.World;
+
+/**
+ * Created by Crystall on 10/30/2019
+ */
+public class SkillDispel extends Skill {
+
+  public SkillDispel(String id, Type type, EntityClass pclass, int[] levelCosts, int[] powerCost, double[] cooldown,
+      Object[] description) throws IllegalArgumentException {
+    super(id, type, pclass, levelCosts, powerCost, cooldown, description);
+  }
+
+  @Override
+  public String getUserFriendlyName(int level) {
+    return null;
+  }
+
+  @Override
+  public String getActivationTime() {
+    return null;
+  }
+
+  @Override
+  public boolean onSkillUse(World world, CombatEntity ce, int level) {
+    return false;
+  }
+}
